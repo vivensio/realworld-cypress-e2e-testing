125c5c9bb380b3302b579d79c19bacf827fa135f
Vivek Patel <vivek.patel@netguru.pl>
2018-09-16 13:37:20 +0200

    add article view tests

---
+++ cypress/integration/article/article_view.spec.js
@@ -0,0 +1,22 @@
+import {
+  ARTICLE_TITLE,
+  ARTICLE_BODY,
+  ARTICLE_TAG,
+} from './constants';
+
+describe('Article Page', () => {
+  beforeEach(() => {
+    cy.login('test123@test.com', 'test123');
+    cy.visit('/#/@test123');
+
+    cy.get('[data-cy="article-title"]').click();
+  });
+
+  context('view article', () => {
+    it('has article title, description and tags in proper places', () => {
+      cy.get('[data-cy="article-title"]').should('have.text', ARTICLE_TITLE);
+      cy.get('[data-cy="article-body"]').should('contain', ARTICLE_BODY);
+      cy.get('[data-cy="article-tag"]').should('contain', ARTICLE_TAG);
+    });
+  });
+});
