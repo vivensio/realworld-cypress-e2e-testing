a34863ecbd217f5a282da1cb5de1026fc4f2aa84
Vivek Patel <vivek.patel@netguru.pl>
2018-09-16 13:37:08 +0200

    Add new article page tests

---
+++ cypress/integration/article/article_new.spec.js
@@ -0,0 +1,50 @@
+import {
+  ARTICLE_TITLE,
+  ARTICLE_DESCRIPTION,
+  ARTICLE_BODY,
+  ARTICLE_TAG,
+  ARTICLE_TAGS,
+} from './constants';
+
+describe('New Article Page', () => {
+  beforeEach(() => {
+    cy.login('test123@test.com', 'test123');
+    cy.visit('/#/editor');
+  });
+
+  context('Tags', () => {
+    it('adds tags', () => {
+      ARTICLE_TAGS.forEach((tag) => {
+        cy.get('[data-cy="article-tags-input"]').type(`${tag}{enter}`);
+        cy.get('[data-cy="article-tags"]').should('contain', tag);
+      });
+
+      cy.get('[data-cy="article-tags"]').should('have.length', ARTICLE_TAGS.length);
+    });
+
+    it('removes tags', () => {
+      ARTICLE_TAGS.forEach((tag) => {
+        cy.get('[data-cy="article-tags-input"]').type(`${tag}{enter}`);
+      });
+
+      cy.get('[data-cy="article-tags"]').each(($el) => {
+        $el.find('[data-cy="article-tag-remove"]').click();
+      });
+
+      cy.get('[data-cy="article-tags"]').should('have.length', 0);
+    });
+  });
+
+  context('New Article', () => {
+    it.skip('posts a new article', () => {
+      cy.get('[data-cy="article-title"]').type(ARTICLE_TITLE);
+      cy.get('[data-cy="article-description"]').type(ARTICLE_DESCRIPTION);
+      cy.get('[data-cy="article-body"]').type(ARTICLE_BODY);
+      cy.get('[data-cy="article-tags-input"]').type(`${ARTICLE_TAG}{enter}`);
+
+      cy.get('[data-cy="article-form"]').submit();
+
+      cy.url().should('contain', ARTICLE_TITLE);
+    });
+  });
+});
---
+++ cypress/integration/article/constants.js
@@ -0,0 +1,5 @@
+export const ARTICLE_TITLE = 'I am the title';
+export const ARTICLE_DESCRIPTION = 'I am the description';
+export const ARTICLE_BODY = 'I am the article body. Look at me!';
+export const ARTICLE_TAG = 'coding';
+export const ARTICLE_TAGS = ['social', 'language', 'psychology', 'travel'];
