7c520feb012e5f41386d9d8e1a380b18b1e506a8
Vivek Patel <vivek.patel@netguru.pl>
2018-09-21 21:05:04 +0200

    Add logout command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,3 +12,9 @@ Cypress.Commands.add('login', (email, password) => (
     window.localStorage.setItem('id_token', response.body.user.token);
   })
 ));
+
+Cypress.Commands.add('logout', () => {
+  cy.window().then((win) => {
+    win.__store__.dispatch('logout'); // eslint-disable-line no-underscore-dangle
+  });
+});
