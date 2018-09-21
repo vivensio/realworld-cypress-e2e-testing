beb9f38f09e20721804b661cb63eb84a8d226645
Vivek Patel <vivek.patel@netguru.pl>
2018-09-21 21:05:38 +0200

    Add comment and favorite tests

--- cypress/integration/article/article_view.spec.js
+++ cypress/integration/article/article_view.spec.js
@@ -2,17 +2,20 @@ import {
   ARTICLE_TITLE,
   ARTICLE_BODY,
   ARTICLE_TAG,
+  COMMENT,
 } from '../../support/constants';
 import { deleteAllArticles, getAllArticles, postArticle } from '../../support/utils';
 
+const setup = () => {
+  cy.login('test123@test.com', 'test123')
+    .then(deleteAllArticles)
+    .then(postArticle);
+  cy.visit('/@test123');
+  cy.get('[data-cy="article-title"]').click();
+};
+
 describe('Article Page', () => {
-  beforeEach(() => {
-    cy.login('test123@test.com', 'test123')
-      .then(deleteAllArticles)
-      .then(postArticle);
-    cy.visit('/@test123');
-    cy.get('[data-cy="article-title"]').click();
-  });
+  beforeEach(setup);
 
   context('view article', () => {
     it('has article title, description and tags in proper places', () => {
@@ -28,5 +31,41 @@ describe('Article Page', () => {
       getAllArticles()
         .should('have.length', 0);
     });
+  });
+});
+
+describe('Article Page - as another user', () => {
+  before(() => {
+    setup();
+    cy.logout();
+    cy.login('test1234@test.com', 'test1234');
+    cy.reload();
+  });
+
+  context('Article actions', () => {
+    it('can post a comment on an article', () => {
+      cy.get('[data-cy="comment-textbox"]').type(COMMENT, { delay: 100 });
+      cy.get('[data-cy="post-comment-btn"]').click();
+      cy.get('[data-cy="comment-body"]').should('contain', COMMENT);
+    });
+
+    it.only('can favorite/unfavorite an article', () => {
+      cy.get('[data-cy="fav-unfav-btn"]').eq(0).should('contain', 'Favorite Article');
+
+      cy.get('[data-cy="fav-unfav-btn"]').eq(0).click();
+
+      cy.get('[data-cy="fav-unfav-btn"]').eq(0).should('contain', 'Unfavorite Article');
+      cy.get('[data-cy="fav-counter"]').should('contain', '(1)');
+
+      cy.get('[data-cy="fav-unfav-btn"]').eq(0).click();
+
+      cy.get('[data-cy="fav-unfav-btn"]').eq(0).should('contain', 'Favorite Article');
+      cy.get('[data-cy="fav-counter"]').should('contain', '(0)');
+    });
+  });
+
+  after(() => {
+    cy.login('test123@test.com', 'test123')
+      .then(deleteAllArticles);
   });
 });
