25929c50bfe252fba300b979686e35ef9ef6e8c2
Vivek Patel <vivek.patel@netguru.pl>
2018-10-21 13:41:06 +0200

    Add node script to reset database

---
+++ reset-db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/conduit');
+
+const dropCollection = async (collection) =>
+ await mongoose.connection.db.dropCollection(collection.collectionName);
+
+mongoose.connection.on('open', async () => {
+  const collections = await mongoose.connection.db.collections();
+  await Promise.all(collections.map(dropCollection));
+  process.exit();
+});
